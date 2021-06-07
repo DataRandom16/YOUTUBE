43f86a843d0e3a735ec1e2bb6c490a2505d1f5a1
Pushkar Shah <64643576+DataRandom16@users.noreply.github.com>
2021-06-07 10:35:49 +0530

    v1.0.0.0

---
+++ simple.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 10:35:04 2021
+
+@author: LENOVO
+"""
+
+
+import random
+import logging
+
+space ="  "
+
+chars= "abcdefghikklmnopqrstuvwxyzABCDEFGHOJKLMNOPQRSTUVWXYZ123456789!@#$%^&*?<>"
+
+
+
+
+logging.basicConfig(filename ="logs.txt",
+                    filemode="a",
+                    format= '%(asctime)s %(levelname)-s%(message)s',
+                    datefmt='%y-%m-%d %H:%M:%S')
+                    
+                 
+work="yes"
+
+
+
+
+while work == "Yes" or work == "yes" :
+    
+    
+    password_len = int(input("What is the length of your password :"))
+    
+    subject= input("For what are you generating password : ")
+    password = ""
+    
+        
+    for x in range (0,password_len):
+        password_char = random.choice(chars)
+        password = password + password_char
+    
+    
+   
+    
+    print("Here is your password for your" , subject , "account " , password )
+    logging.warning(" Here is your password for your " + subject + " account" + str(space) +  str(password))
+    
+    work = input("Do you want to continue : ")
+
