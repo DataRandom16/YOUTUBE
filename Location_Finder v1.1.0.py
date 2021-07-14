6d0a05b39857a3c34e450aeadc2c3a5fd6c2de19
Pushkar Shah <64643576+DataRandom16@users.noreply.github.com>
2021-07-14 08:48:14 +0530

    Published : 14-07-21 [v1.1.0]

---
+++ Location_Finder v1.1.0.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 12:18:17 2021
+
+@author: LENOVO
+"""
+
+
+#importing_Libs
+#---------------------------------#
+
+import phonenumbers
+import folium 
+from phonenumbers import geocoder
+from phonenumbers import carrier
+from opencage.geocoder import OpenCageGeocode
+
+#Asking_for_Phone_Number
+#---------------------------------#
+number = input("Enter your phone Number : ")
+
+#Getting_API_key
+#---------------------------------#
+key = '96b631a81575445b80627bde637f3082'
+
+
+#Getting_The_Country
+#---------------------------------#
+get_number = phonenumbers.parse(number)
+location = geocoder.description_for_number(get_number , "en")
+print(location)
+
+
+#Getting_The_Service_provider_details
+#---------------------------------#
+provider = phonenumbers.parse(number)
+print(carrier.name_for_number(provider, "en"))
+
+
+#Getting_The_Actual_location(Not Accurate)
+#---------------------------------#
+geocoder= OpenCageGeocode(key)
+asked = str(location)     
+result = geocoder.geocode(asked)
+
+#Filtering_the_Data
+#---------------------------------#
+lat = result[0]['geometry']['lat']
+long = result[0]['geometry']['lng']
+
+#Printing_the_latitudnal_and_longitudnal_locaton(Not Accurate)
+#---------------------------------#
+print(lat,long)
+
+
+#Making_A_Map(.html)
+#---------------------------------#
+map_my = folium.Map(location=[lat , long ] , zoom_start= 9 )
+folium.Marker([lat , long ], popup = location).add_to((map_my))
+
+
+#Saving_the_map(.html)
+#---------------------------------#
+map_my.save("Map.html")
